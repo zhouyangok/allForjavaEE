9407fba48db571ad8e313c75dea5f74526c639f3
zhouyangok <zhouyangx99@163.com>
2019-05-01 21:20:06 +0800

    first commit

---
+++ src/main/java/com/crazyang/JavaApplication.java
@@ -0,0 +1,15 @@
+package com.crazyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+
+@SpringBootApplication
+public class JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApplication.class, args);
+	}
+
+}
